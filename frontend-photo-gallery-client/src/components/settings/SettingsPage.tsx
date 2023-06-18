e2491d535a3e0df6ab1ebc88bf42646774b7ba39
terary <terary@gmail.com>
2023-06-18 01:25:49 -0500

    Removed broken image editor

--- frontend-photo-gallery-client/src/components/settings/SettingsPage.tsx
+++ frontend-photo-gallery-client/src/components/settings/SettingsPage.tsx
@@ -1,12 +1,11 @@
 import { ImageEnvelope } from "./images/ImageEnvelope";
 import AxiosCall from "../AxiosCall";
-import { MyComponent } from "./images/Editor/Editor";
+
 export const SettingsPage = () => {
   return (
     <div>
       AxiosCall
       <AxiosCall />
-      <MyComponent />
       {/* <ImageEnvelope /> */}
     </div>
   );
