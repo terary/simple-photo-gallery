9032e55acce1df06711edce2c7032999de8d886b
terary <terary@gmail.com>
2023-06-18 00:58:01 -0500

    house cleaning

--- frontend-photo-gallery-client/frontend-photo-gallery-client.to.be-deleted/src/components/SlideShow/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FadeShow = exports.SlideShow = void 0;
-var SlideShow_1 = require("./SlideShow");
-Object.defineProperty(exports, "SlideShow", { enumerable: true, get: function () { return SlideShow_1.SlideShow; } });
-var FadeShow_1 = require("./FadeShow");
-Object.defineProperty(exports, "FadeShow", { enumerable: true, get: function () { return FadeShow_1.FadeShow; } });
-//# sourceMappingURL=index.js.map
--- frontend-photo-gallery-client/frontend-photo-gallery-client.to.be-deleted/src/components/SlideShow/index.ts
+++
@@ -1,2 +0,0 @@
-export {SlideShow} from "./SlideShow"
-export {FadeShow} from "./FadeShow"
